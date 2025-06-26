415b475c0fe9847919f1e19611a5661c87157f34
j0rger1vera <96147065+j0rger1vera@users.noreply.github.com>
2025-06-26 08:33:07 -0500

    Update FacturacionGeneralDTO.java

--- src/main/java/com/facturacion/dto/FacturacionGeneralDTO.java
+++ src/main/java/com/facturacion/dto/FacturacionGeneralDTO.java
@@ -1,5 +1,9 @@
 package com.facturacion.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +14,13 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 public class FacturacionGeneralDTO {
+        private Integer idFactura;
         private Integer numeroFactura;
         private String nitCliente;
         private String nombreCliente;
         private BigDecimal saldo;
         private BigDecimal abono;
         private BigDecimal total;
+        private String detalle;
         private String fechaFacturada;
 }
